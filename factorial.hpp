b8ae86a1469470e34c8c8f01213c34c92d4527dc
ConnorJ88 <123974095+ConnorJ88@users.noreply.github.com>
2025-10-20 14:59:43 +0000

    Bank Account and Factorial pass test

--- factorial.hpp
+++ factorial.hpp
@@ -2,8 +2,14 @@
 
 // Function to calculate factorial using multiple threads
 long calculateFactorial(int n) {
-    long result = 0;
+    long result = 1;
+    if (n < 0) {
+        throw std::invalid_argument("Factorial is not defined for negative numbers");
+    }
     //TODO: Implement the function to calculate factorial of n
-
+    for(int i; i<n+1; i++)
+        {
+            result *= i;
+        }
     return result;
 }
